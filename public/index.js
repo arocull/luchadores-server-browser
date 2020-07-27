66c92094c4790df6e59b617bbade00d974481127
James O'Cull <james.ocull@genesys.com>
2020-07-26 22:28:06 -0400

    Finally fixed the table version

--- public/index.js
+++ public/index.js
@@ -1,7 +1,8 @@
 // 😭 but my global scope 😭
 // Temporary hacks because I'm too lazy/scared to setup webpack
-(function() {
-  const tblMainBody = $("#tableId > tbody");
+$(function() {
+  const btnRefresh = $('#btn-refresh');
+  const tblBody = $('#tbl-main > tbody');
 
   function getServers() {
     return fetch('/list')
@@ -9,33 +10,41 @@
   }
 
   function updateServerList() {
-    getServers()
+    Promise.resolve()
+      .then(() => {
+        tblBody.empty();
+        btnRefresh.attr('disabled', 'disabled');
+        const row = $('<tr>');
+        const cell = $('<td>').attr('colspan', 2).text('Refreshing...');
+        row.append(cell);
+        tblBody.append(row);
+        return getServers();
+      })
       .then(data => {
         console.log('Got server list', data);
-
-        // TODO: Something is wrong with this still
-        tblMainBody.html(""); // idk why -- https://stackoverflow.com/a/16444715/97964
+        btnRefresh.removeAttr('disabled');
+        tblBody.empty();
         data.forEach(entry => {
-          const link = $('a').attr('href', entry.address).text(entry.title);
-          const row = $('tr');
-          const cellName = $('td');
+          const link = $('<a>').attr('href', entry.address).text(entry.title);
+          const row = $('<tr>');
+          const cellName = $('<td>');
           cellName.append(link);
-          const cellPlayers = $('td');
-          cellPlayers.text(`{entry.playerCount} / {entry.playerCapacity}`);
+          const cellPlayers = $('<td>');
+          cellPlayers.text(`${entry.playerCount} / ${entry.playerCapacity}`);
           row.append(cellName);
           row.append(cellPlayers);
-          tblMainBody.append(row);
+          tblBody.append(row);
         });
         if (data.length == 0) {
-          const row = $('tr');
-          const cell = $('td').attr('colspan', 2).text('No servers 😭');
+          const row = $('<tr>');
+          const cell = $('<td>').attr('colspan', 2).text('No servers 😭');
           row.append(cell);
-          tblMainBody.append(row);
+          tblBody.append(row);
         }
       });
   }
 
   // Run initially and again if refreshed
   updateServerList();
-  $('#btn-refresh').click(updateServerList);
-})();
+  btnRefresh.click(updateServerList);
+});
